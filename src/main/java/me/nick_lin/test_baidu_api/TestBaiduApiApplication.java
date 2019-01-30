a449a41aacee6f362e5e4b7f51e1b88fbec4384d
Nick-Lcy <nicklin9907@gmail.com>
2019-01-30 23:42:10 +0800

    Ini Project

---
+++ src/main/java/me/nick_lin/test_baidu_api/TestBaiduApiApplication.java
@@ -0,0 +1,14 @@
+package me.nick_lin.test_baidu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBaiduApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBaiduApiApplication.class, args);
+    }
+
+}
+
